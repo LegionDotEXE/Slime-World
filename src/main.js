a4fce332bf6d8f5b76eaba2c26b80ce196c0013e
Saurab S <ajay.v.v.743@gmail.com>
2026-02-12 18:24:01 -0800

    Initial

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: Slime World
+// Name: Saurav Shah    
+// Date: 02/11/2026
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
